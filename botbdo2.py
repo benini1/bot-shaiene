d7ea9604e0eab475bdb48ade49a2b86fce4d1795
benini1 <beniniwp@gmail.com>
2020-07-06 15:26:34 -0300

    env var

--- botbdo2.py
+++ botbdo2.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 import config
 
-token = os.environ.get('token')
+token = os.environ('token')
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
