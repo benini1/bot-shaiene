23001ded8aa87b7e51b63b973031203e739b4ad9
benini1 <beniniwp@gmail.com>
2020-07-06 14:50:51 -0300

    ajuste token

--- botbdo2.py
+++ botbdo2.py
@@ -1,8 +1,10 @@
+from multiprocessing import process
+
 import discord
 from discord.ext import commands
 import config
 
-token = config.token
+token = process.env.token
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
